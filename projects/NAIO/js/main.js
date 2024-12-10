b665aeadc145df20f2912867ee23c39894e1c5d6
montderakeel <montder.akeel101@gmail.com>
2024-12-10 18:12:18 +0300

    Update main.js
    side navbar and loading

--- projects/NAIO/js/main.js
+++ projects/NAIO/js/main.js
@@ -1,6 +1,42 @@
 (function () {
   "use strict";
 
+  // Loading section
+  document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      document.body.classList.add("loaded");
+    }, 1000);
+  });
+
+  // Side Navbar
+  const SideNaveBtn = document.getElementById("sideNaveBtn"),
+    SideNave = document.getElementById("sideNave");
+
+  // Initially hide the side navigation
+  SideNave.style.right = "-240px"; // Start off-screen
+
+  // Toggle button click
+  SideNaveBtn.onclick = function (event) {
+    event.stopPropagation(); // Prevent the click from propagating to the document
+    if (SideNave.style.right === "-240px") {
+      // Show side navigation
+      SideNave.style.right = "0px";
+    } else {
+      // Hide side navigation
+      SideNave.style.right = "-240px";
+    }
+  };
+
+  // Close side navigation when clicking outside
+  document.onclick = function () {
+    SideNave.style.right = "-240px"; // Hide the side navigation
+  };
+
+  // Prevent closing when clicking inside the side navigation
+  SideNave.onclick = function (event) {
+    event.stopPropagation(); // Prevent the click from propagating to the document
+  };
+
   AOS.init();
 
   const swiper = new Swiper(".mySwiper", {
